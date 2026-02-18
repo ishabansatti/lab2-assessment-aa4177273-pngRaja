8f0dcb592342dd5c2c16360970180cbe8e406b5e
aa4177273-pngRaja <aa4177273@gmail.com>
2026-02-18 01:52:09 -0800

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -4,17 +4,21 @@ using namespace std;
 int getIntValue() {
     // Declare a variable var_int of type int and set its value to 10.
     int var_int = 10;
-    return var_int; //this is a code
+    return var_int; 
 }
 
 float getFloatValue() {
     // Declare a variable var_float of type float and set its value to 10.75.
     //return var_float;
+    float var_float =10.17;
+    return var_float;
 }
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
+    char var_char =A;
+    return var_char;
 }
 
 int convertFloatToInt(float value) {
